f659e11f7758767397dfc40e6ec28e57ee5fa486
LiuRivers <zhaoxiang0805@hotmail.com>
2017-12-15 21:43:31 +0800

    面向对象

---
+++ questions/backup_ver4.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+source = ['/Users/Rivers/Rivers/test']
+
+target_dir = '/Users/Rivers/Rivers/backup'
+
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+today = target_dir + os.sep + time.strftime('%Y%m%d');
+
+now = time.strftime('%H%M%S')
+
+# 以下命令在Mac版会报错，直接命令行却可以运行
+comment = input('Enter a comment -->')
+
+if len(comment) == 0:
+    target = today + os.sep + now + '.zip'
+else:
+    target = today + os.sep + now + '_' + \
+             comment.replace(' ', '_') + '.zip'
+
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successfully created directory', today)
+
+zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
+
+print('Zip command is:')
+print(zip_command)
+print('Running...')
+if os.system(zip_command) == 0:
+    print('Successfully backup to ' + target)
+else:
+    print('Backup Failed')
